4ddeb4731cc420aeebdefb1cb0cb2e5c0b8cf667
Vanderson Luis <vandersonlb@hotmail.com>
2022-04-20 14:28:30 -0300

    day 4

--- helpers/DateFormat.js
+++ helpers/DateFormat.js
@@ -1,7 +1,14 @@
-export default function formattedDate(birthdate) {
+function UStoBR(birthdate) {
   let date = new Date(birthdate.split("-"));
   let day = date.getDate().toString().padStart(2, "0");
   let month = (date.getMonth() + 1).toString().padStart(2, "0");
   let year = date.getFullYear();
   return `${day}/${month}/${year}`;
 }
+
+function BRtoUS(birthdate) {
+  let split = birthdate.split("/");
+  return `${split[2]}-${split[1]}-${split[0]}`
+}
+
+export default {UStoBR,  BRtoUS}
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-import dateFormat from "./helpers/DateFormat.js"
+import DateFormat from "./helpers/DateFormat.js"
 
 const form = document.querySelector(".js-form");
 const table = document.querySelector(".js-table");
+
+const model = (name, birthdate) => ({ name, birthdate })
 const pessoas = JSON.parse(localStorage.getItem("pessoas")) || [];
 
 tableView(pessoas, table)
@@ -9,14 +11,13 @@ tableView(pessoas, table)
 form.onsubmit = (evt) => {
   evt.preventDefault();
   const { name, birth_date } = form;
-  insertData({ name: name.value, birthdate: birth_date.value });
+  insertUser(model(name.value, birth_date.value));
   tableView(pessoas, table);
   form.reset()
 };
 
 (function handleValidity() {
   let inputs = form.querySelectorAll("input");
-
   inputs.forEach((input) => {
     input.oninvalid = () => {
       input.setCustomValidity("");
@@ -25,14 +26,27 @@ form.onsubmit = (evt) => {
   });
 })();
 
-function insertData(user) {
+function tableView(modelList, view) {
+  var tableBody = view.querySelector("tbody");
+  tableBody.innerHTML = modelList
+  .map((user, idx) => `<tr id="${idx}"><td contenteditable>${user.name}</td> <td contenteditable>${DateFormat.UStoBR(user.birthdate)}</td></tr>`)
+  .join("");
+}
+
+table.addEventListener("focusout", (evt) => {
+  let id = evt.target.parentNode.id;
+  let name = evt.target.parentNode.children[0].innerHTML;
+  let birth_date = evt.target.parentNode.children[1].innerHTML;
+  updateUser(pessoas, id, model(name, DateFormat.BRtoUS(birth_date)))
+})
+
+function insertUser(user) {
   pessoas.push(user);
   localStorage.setItem("pessoas", JSON.stringify(pessoas));
 }
 
-function tableView(model, view) {
-  var tableBody = view.querySelector("tbody");
-  tableBody.innerHTML = model
-    .map((user) => `<tr><td>${user.name}</td> <td>${dateFormat(user.birthdate)}</td></tr>`)
-    .join("");
+function updateUser(modelList, id, userUpdated) {
+  modelList.splice(id, 1, userUpdated)
+  localStorage.setItem("pessoas", JSON.stringify(pessoas));
+  tableView(pessoas, table)
 }
